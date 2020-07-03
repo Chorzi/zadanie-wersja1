58ba5f270d3fb6518c685c877f67089d1314e7fc
Chorzi <chorzi53@gmail.com>
2020-07-03 13:41:29 +0200

    Updates

---
+++ precache-manifest.ddfe923b05faa0aefc57ad4c694c43ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af902ecf2234c9e5429f556c4df4b311",
+    "url": "/http//Chorzi.github.io/zadanie-wersja2/index.html"
+  },
+  {
+    "revision": "4ef0eed182b13d9c8c95",
+    "url": "/http//Chorzi.github.io/zadanie-wersja2/static/css/main.65deed25.chunk.css"
+  },
+  {
+    "revision": "bb8105ca8efb84e14280",
+    "url": "/http//Chorzi.github.io/zadanie-wersja2/static/js/2.b752b209.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/http//Chorzi.github.io/zadanie-wersja2/static/js/2.b752b209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ef0eed182b13d9c8c95",
+    "url": "/http//Chorzi.github.io/zadanie-wersja2/static/js/main.9d59fa3c.chunk.js"
+  },
+  {
+    "revision": "60e5b753121361cbee33",
+    "url": "/http//Chorzi.github.io/zadanie-wersja2/static/js/runtime-main.2d4b21d8.js"
+  }
+]);
